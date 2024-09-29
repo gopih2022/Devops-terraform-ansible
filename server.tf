ad6482c4909681280e77c84215f314b8c9ec038e
gopih2022 <161722024+gopih2022@users.noreply.github.com>
2024-09-29 17:31:52 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -12,7 +12,7 @@ region = "us-east-1"
 }
 resource "aws_instance" "myserver" {
   ami = "ami-0e86e20dae9224db8"
-  vpc_security_group_ids  = [aws_security_group.gopi-sg.id]
+  vpc_security_group_ids  = [aws_security_group.gopi_sg.id]
   instance_type = "t2.micro"
   key_name = "gopi-role"
 
